845ab856b518cb5e29222d947c6cc0ce826f7f43
Austin West <korruptedanomaly@gmail.com>
2019-06-14 09:59:33 -0600

    Just an update

--- Assessment1Notes/Assessment1Notes/CONTROLLERS/MODEL CONTROLLER/NoteController.swift
+++ Assessment1Notes/Assessment1Notes/CONTROLLERS/MODEL CONTROLLER/NoteController.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class NoteController {
+    
+    // Singleton
+    static let shared = NoteController()
+    
+    // Source of Truth
+    var notes: [Note] = []
+    
+    func addEntryWith(title: String, text: String) {
+        // Appending/adding the note to the notes source of truth, which applies it to the Note model
+        let note = Note(noteTitle: title, noteText: text)
+        notes.append(note)
+    }
+    
+    func remove(note: Note) {
+        // Searches for an index point/location of an entry and then removes it from the notes array
+        if let noteIndex = notes.firstIndex(of: note) {
+            notes.remove(at: noteIndex)
+        }
+    }
+    
+    func update(note: Note, with title: String, text: String) {
+        
+        note.noteTitle = title
+        note.noteText = text
+    }
+    
+    
+    
+}
